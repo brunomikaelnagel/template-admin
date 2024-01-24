b91e8240923ed7b43922f07d19fdc2e9e6e3ba52
Bruno9112 <brunonagel9132@gmail.com>
2024-01-23 22:14:34 -0300

    atualizacão de metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'template-admin',
-  description: 'template-admin feito com firebase e nextjs',
+  description: 'Um template administrativo responsivo desenvolvido com Next.js, integrado com autenticação Firebase. Este template oferece uma interface de usuário limpa e moderna para a administração de dados e recursos.'
 }
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,6 @@
 // Components
 import LayoutApp from '@/components/LayoutApp'
 
-// CSS
-import styles from './page.module.css'
-
 export default function Home() {
   return (
     <LayoutApp title='Página inicial' subtitle='Estamos contruindo um template-admin' >
